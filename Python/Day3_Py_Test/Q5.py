14383a4ab63e7050d7dc39731f269a81fbe7f822
Akashdeep Singh <aksinghsidhu07@gmail.com>
2026-06-04 17:40:46 +0530

    Fixed the issues according to questions.

--- Python/Day3_Py_Test/Q5.py
+++ Python/Day3_Py_Test/Q5.py
@@ -1,17 +1,18 @@
 # Write `convert_temp(value, from_unit, to_unit)` that converts between Celsius, Fahrenheit, and Kelvin. Store each conversion formula as a lambda in a dictionary, look it up and apply it. Test with at least 4 different combinations.
 
-convert_temp = {
-    "c2f" : lambda temp: temp * (9 / 5) + 32,
-    "f2c" : lambda temp: (temp - 32) * (5 / 9),
-    "c2k" : lambda temp: temp + 273.15,
-    "k2c" : lambda temp: temp - 273.15,
-    "f2k" : lambda temp: (temp -32) * (5 / 9) + 273.15,
-    "k2f" : lambda temp: (temp - 273.15) * (9 / 5) + 32
-}
+def convert_temp(value, from_unit, to_unit):
+    conversions = {
+        "c2f" : lambda temp: temp * (9 / 5) + 32,
+        "f2c" : lambda temp: (temp - 32) * (5 / 9),
+        "c2k" : lambda temp: temp + 273.15,
+        "k2c" : lambda temp: temp - 273.15,
+        "f2k" : lambda temp: (temp -32) * (5 / 9) + 273.15,
+        "k2f" : lambda temp: (temp - 273.15) * (9 / 5) + 32
+        }
+    key = from_unit + "2" + to_unit
+    return conversions[key](value)
 
-print(convert_temp["c2f"](5))
-print(convert_temp["f2c"](5))
-print(convert_temp["c2k"](5))
-print(convert_temp["k2c"](5))
-print(convert_temp["f2k"](5))
-print(convert_temp["k2f"](5))
+print(convert_temp(5, "c", "f"))
+print(convert_temp(5, "f", "c"))
+print(convert_temp(5, "c", "k"))
+print(convert_temp(5, "k", "c"))
--- Python/Day3_Py_Test/Q6.py
+++ Python/Day3_Py_Test/Q6.py
@@ -3,21 +3,20 @@
 def text_analyzer(text):
     arr = text.split()
     str1 = text.lower().replace(" ", "")
-    print("Number of words:", len(arr))
-    print("Number of Characters without spaces:", len(str1))
     my_dict = {}
     for x in str1:
         my_dict[x] = str1.count(x)
+    dictionary = {"No. of words" : len(arr),
+                  "No. of Characters without spaces" : len(str1),
+                  "Most common letter (case-insensitive)" : max(my_dict, key = str1.count),
+                  "Count of each vowel" : {"a" : str1.count("a"),
+                                           "e": str1.count("e"),
+                                           "i": str1.count("i"),
+                                           "o": str1.count("o"),
+                                           "u": str1.count("u"),
+                                           }
+                  }
+    return dictionary
 
-    print("Most common letter (Case Insensitive):", max(my_dict, key = str1.count) + " repeating " , max(my_dict.values()) , "times")
-    countVowels = (
-        str1.count("a")
-        + str1.count("e")
-        + str1.count("i")
-        + str1.count("o")
-        + str1.count("u")
-    )
-    print("Total Number of Vowels:", countVowels)
-
-str = "I am a Btech Student"
-text_analyzer(str)
+sentence = "I am a Btech Student"
+print(text_analyzer(sentence))
